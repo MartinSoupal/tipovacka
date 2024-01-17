cc637290edfddccc1f617112fac4ba98dacb6d04
Martin šoupal <m.soupal.06@gmail.com>
2024-01-17 22:10:15 +0100

    init ESLint and run it on pull request

--- tipovacka-app/src/app/utils/arrayToHashMap.fnc.ts
+++ tipovacka-app/src/app/utils/arrayToHashMap.fnc.ts
@@ -1,4 +1,2 @@
-// @ts-ignore
-export const arrayToHashMap = <T extends object, K extends keyof T>(key: K, array: T[]): Record<T[K], T> =>
-  // @ts-ignore
+export const arrayToHashMap = <T extends Record<K, string | number>, K extends keyof T>(key: K, array: T[]): Record<T[K], T> =>
   array.reduce((acc, item) => Object.assign(acc, {[item[key]]: item}), {} as Record<T[K], T>);
