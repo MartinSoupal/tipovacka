4cb9cd4d084d2ed244431de74c4dd2bf155a8437
Martin šoupal <m.soupal.06@gmail.com>
2024-02-21 19:34:04 +0100

    button redesign

--- tipovacka-app/src/app/components/match/match.component.ts
+++ tipovacka-app/src/app/components/match/match.component.ts
@@ -50,6 +50,7 @@ export class MatchComponent implements OnChanges, OnInit {
     0: false,
     2: false,
   }
+  protected readonly undefined = undefined;
   private dataService = inject(DataService);
   private dialogService = inject(DialogService);
 
